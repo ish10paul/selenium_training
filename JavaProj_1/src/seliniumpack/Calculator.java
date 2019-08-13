4f40026b01bb4acf4a62329392bff25c609f98bb
Training_B4C.05.15 <Training_B4C.05.15@BDC7-D-1SR7N62.dir.svc.accenture.com>
2019-08-13 17:04:29 +0530

    commit1

---
+++ JavaProj_1/src/seliniumpack/Calculator.java
@@ -0,0 +1,11 @@
+package seliniumpack;
+
+public class Calculator {
+	public static void main(String[] args) {
+		int v1=10;
+		int v2=20;
+		int res=v1+v2;
+		System.out.println("total mark is: "+res);
+	}
+
+}
